758bc04ff3aedb6681ea30d2168828a1835cd33f
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-26 15:33:11 +0200

    do header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,32 @@
-<header>
-  <div class="container-fluid bg-primary">
+@php
+  $menu = [
+    ["title" => "Characters"],
+    ["title" => "Comics"],
+    ["title" => "Movies"],
+    ["title" => "Tv"],
+    ["title" => "Games"],
+    ["title" => "Collectibles"],
+    ["title" => "Videos"],
+    ["title" => "Fans"],
+    ["title" => "News"],
+    ["title" => "Shop"]
+  ];
+@endphp
 
+<header>
+  <div class="container-fluid">
+    <div class="container d-flex justify-content-between align-items-center py-4">
+      <img id="logo" src="../../vue-dc-comics-1/img/dc-logo.png" alt="DC Logo">
+
+      <ul class="nav nav-pills">
+        @foreach ($menu as $item)
+          <li class="nav-item" v-for="link in navLinks">
+            <a href="#" class="nav-link text-black fw-bold fs-6">
+              {{ $item["title"] }}
+            </a>
+          </li>
+        @endforeach        
+      </ul>
+    </div>
   </div>
 </header>
